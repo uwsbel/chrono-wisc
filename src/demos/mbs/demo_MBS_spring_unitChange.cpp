267ab851d867549a5848345f39e3543599e62a9e
Huzaifg <huzaifa.unjha@gmail.com>
2024-11-06 11:31:11 -0600

    unit change test with spring

--- src/demos/mbs/demo_MBS_spring_unitChange.cpp
+++ src/demos/mbs/demo_MBS_spring_unitChange.cpp
@@ -42,13 +42,18 @@ using namespace chrono::vsg3d;
 
 using namespace chrono;
 
+// Unit multipler - 0.004 mm now corresponds to 1 unit of length
+// Thus 1 m corresponds to 250 units of length
+// so all m values should be multiplied by 250
+double unit_multiplier = 250;
+
 // =============================================================================
 
 ChVisualSystem::Type vis_type = ChVisualSystem::Type::VSG;
 
-double rest_length = 1.5;
-double spring_coef = 50;
-double damping_coef = 1;
+double rest_length = 1.5 * unit_multiplier;
+double spring_coef = 50;  // no change
+double damping_coef = 1;  // no change
 
 // =============================================================================
 
@@ -84,10 +89,10 @@ int main(int argc, char* argv[]) {
 
     {
         auto sph_1 = chrono_types::make_shared<ChVisualShapeSphere>(0.1);
-        ground->AddVisualShape(sph_1, ChFrame<>(ChVector3d(-1, 0, 0), QUNIT));
+        ground->AddVisualShape(sph_1, ChFrame<>(ChVector3d(-1 * unit_multiplier, 0, 0), QUNIT));
 
         auto sph_2 = chrono_types::make_shared<ChVisualShapeSphere>(0.1);
-        ground->AddVisualShape(sph_2, ChFrame<>(ChVector3d(+1, 0, 0), QUNIT));
+        ground->AddVisualShape(sph_2, ChFrame<>(ChVector3d(+1 * unit_multiplier, 0, 0), QUNIT));
     }
 
     // Create a body suspended through a ChLinkTSDA (default linear)
@@ -95,21 +100,23 @@ int main(int argc, char* argv[]) {
 
     auto body_1 = chrono_types::make_shared<ChBody>();
     sys.AddBody(body_1);
-    body_1->SetPos(ChVector3d(-1, -3, 0));
+    body_1->SetPos(ChVector3d(-1 * unit_multiplier, -3 * unit_multiplier, 0));
     body_1->SetFixed(false);
     body_1->EnableCollision(false);
     body_1->SetMass(1);
-    body_1->SetInertiaXX(ChVector3d(1, 1, 1));
+    body_1->SetInertiaXX(ChVector3d(1 * unit_multiplier * unit_multiplier, 1 * unit_multiplier * unit_multiplier,
+                                    1 * unit_multiplier * unit_multiplier));
 
     // Attach a visualization asset.
-    auto box_1 = chrono_types::make_shared<ChVisualShapeBox>(1, 1, 1);
+    auto box_1 =
+        chrono_types::make_shared<ChVisualShapeBox>(1 * unit_multiplier, 1 * unit_multiplier, 1 * unit_multiplier);
     box_1->SetColor(ChColor(0.6f, 0, 0));
     body_1->AddVisualShape(box_1);
 
     // Create the spring between body_1 and ground. The spring end points are
     // specified in the body relative frames.
     auto spring_1 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_1->Initialize(body_1, ground, true, ChVector3d(0, 0, 0), ChVector3d(-1, 0, 0));
+    spring_1->Initialize(body_1, ground, true, ChVector3d(0, 0, 0), ChVector3d(-1 * unit_multiplier, 0, 0));
     spring_1->SetRestLength(rest_length);
     spring_1->SetSpringCoefficient(spring_coef);
     spring_1->SetDampingCoefficient(damping_coef);
@@ -123,14 +130,16 @@ int main(int argc, char* argv[]) {
 
     auto body_2 = chrono_types::make_shared<ChBody>();
     sys.AddBody(body_2);
-    body_2->SetPos(ChVector3d(1, -3, 0));
+    body_2->SetPos(ChVector3d(1 * unit_multiplier, -3 * unit_multiplier, 0));
     body_2->SetFixed(false);
     body_2->EnableCollision(false);
     body_2->SetMass(1);
-    body_2->SetInertiaXX(ChVector3d(1, 1, 1));
+    body_2->SetInertiaXX(ChVector3d(1 * unit_multiplier * unit_multiplier, 1 * unit_multiplier * unit_multiplier,
+                                    1 * unit_multiplier * unit_multiplier));
 
     // Attach a visualization asset.
-    auto box_2 = chrono_types::make_shared<ChVisualShapeBox>(1, 1, 1);
+    auto box_2 =
+        chrono_types::make_shared<ChVisualShapeBox>(1 * unit_multiplier, 1 * unit_multiplier, 1 * unit_multiplier);
     box_2->SetColor(ChColor(0, 0, 0.6f));
     body_2->AddVisualShape(box_2);
 
@@ -139,13 +148,13 @@ int main(int argc, char* argv[]) {
     auto force = chrono_types::make_shared<MySpringForce>();
 
     auto spring_2 = chrono_types::make_shared<ChLinkTSDA>();
-    spring_2->Initialize(body_2, ground, true, ChVector3d(0, 0, 0), ChVector3d(1, 0, 0));
+    spring_2->Initialize(body_2, ground, true, ChVector3d(0, 0, 0), ChVector3d(1 * unit_multiplier, 0, 0));
     spring_2->SetRestLength(rest_length);
     spring_2->RegisterForceFunctor(force);
     sys.AddLink(spring_2);
 
     // Attach a visualization asset.
-    spring_2->AddVisualShape(chrono_types::make_shared<ChVisualShapeSpring>(0.1, 80, 15));
+    spring_2->AddVisualShape(chrono_types::make_shared<ChVisualShapeSpring>(0.1 * unit_multiplier, 80, 15));
 
     // Create the run-time visualization system
     // ----------------------------------------
@@ -170,7 +179,7 @@ int main(int argc, char* argv[]) {
             vis_irr->Initialize();
             vis_irr->AddLogo();
             vis_irr->AddSkyBox();
-            vis_irr->AddCamera(ChVector3d(0, 0, 6));
+            vis_irr->AddCamera(ChVector3d(0, 0, 6 * unit_multiplier));
             vis_irr->AddTypicalLights();
 
             vis = vis_irr;
@@ -187,8 +196,8 @@ int main(int argc, char* argv[]) {
             vis_vsg->SetWindowPosition(ChVector2i(100, 300));
             vis_vsg->SetWindowTitle("Chrono VSG Springs");
             vis_vsg->SetUseSkyBox(true);
-            vis_vsg->AddCamera(ChVector3d(0, 0, 12));
-            vis_vsg->SetCameraAngleDeg(40);
+            vis_vsg->AddCamera(ChVector3d(-200, 200, 12));
+            vis_vsg->SetCameraAngleDeg(90);
             vis_vsg->SetLightIntensity(1.0f);
             vis_vsg->SetLightDirection(1.5 * CH_PI_2, CH_PI_4);
             vis_vsg->SetShadows(true);
@@ -211,13 +220,23 @@ int main(int argc, char* argv[]) {
         vis->EndScene();
 
         if (frame % 100 == 0) {
+            // std::cout << "Time: " << sys.GetChTime() << " s" << std::endl;
+            // std::cout << "Spring 1 length: " << spring_1->GetLength() << " units" << std::endl;
+            // std::cout << "Spring 1 velocity: " << spring_1->GetVelocity() << " units/s" << std::endl;
+            // std::cout << "Spring 1 force: " << spring_1->GetForce() << " kg units/s^2" << std::endl;
+            // std::cout << "Spring 2 length: " << spring_2->GetLength() << " units" << std::endl;
+            // std::cout << "Spring 2 velocity: " << spring_2->GetVelocity() << " units/s" << std::endl;
+            // std::cout << "Spring 2 force: " << spring_2->GetForce() << " kg units/s^2" << std::endl;
+
+            // std::cout << std::endl;
+
             std::cout << "Time: " << sys.GetChTime() << " s" << std::endl;
-            std::cout << "Spring 1 length: " << spring_1->GetLength() << " m" << std::endl;
-            std::cout << "Spring 1 velocity: " << spring_1->GetVelocity() << " m/s" << std::endl;
-            std::cout << "Spring 1 force: " << spring_1->GetForce() << " kg m/s^2" << std::endl;
-            std::cout << "Spring 2 length: " << spring_2->GetLength() << " m" << std::endl;
-            std::cout << "Spring 2 velocity: " << spring_2->GetVelocity() << " m/s" << std::endl;
-            std::cout << "Spring 2 force: " << spring_2->GetForce() << " kg m/s^2" << std::endl;
+            std::cout << "Spring 1 length: " << spring_1->GetLength() / unit_multiplier << " m" << std::endl;
+            std::cout << "Spring 1 velocity: " << spring_1->GetVelocity() / unit_multiplier << " m/s" << std::endl;
+            std::cout << "Spring 1 force: " << spring_1->GetForce() / unit_multiplier << " kg m/s^2" << std::endl;
+            std::cout << "Spring 2 length: " << spring_2->GetLength() / unit_multiplier << " m" << std::endl;
+            std::cout << "Spring 2 velocity: " << spring_2->GetVelocity() / unit_multiplier << " m/s" << std::endl;
+            std::cout << "Spring 2 force: " << spring_2->GetForce() / unit_multiplier << " kg m/s^2" << std::endl;
         }
 
         sys.DoStepDynamics(timestep);
