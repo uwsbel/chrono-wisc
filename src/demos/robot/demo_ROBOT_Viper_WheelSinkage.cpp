dce317918892bc6a7c54e6d63471db709fa2e366
nevindu <nevindubatagoda@gmail.com>
2024-11-21 13:56:55 -0600

    wheel simkage demo updates

--- src/demos/robot/demo_ROBOT_Viper_WheelSinkage.cpp
+++ src/demos/robot/demo_ROBOT_Viper_WheelSinkage.cpp
@@ -235,6 +235,7 @@ class FloatBufferAttrVector {
 #else
 void createVoxelGrid(std::vector<float> points,
                      ChSystemNSC& sys,
+                     ChSystemFsi& sysFSI,
                      std::shared_ptr<ChScene> scene,
                      std::shared_ptr<ChVisualMaterial> vis_mat);
 #endif
@@ -574,7 +575,7 @@ int main(int argc, char* argv[]) {
             #ifdef USE_SENSOR_NVDB
                 createVoxelGrid(h_points, sysMBS, manager->scene, regolith_material);
             #else
-                createVoxelGrid(h_points, sysMBS, manager->scene, regolith_material);
+                createVoxelGrid(h_points, sysMBS, sysFSI, manager->scene, regolith_material);
             #endif
             manager->Update();
         }
@@ -616,6 +617,7 @@ int main(int argc, char* argv[]) {
 void createVoxelGrid(std::vector<float> points, ChSystemNSC& sys, std::shared_ptr<ChScene> scene, std::shared_ptr<ChVisualMaterial> vis_mat) {
     std::cout << "Creating OpenVDB Voxel Grid for " << points.size()/6 << "particles " << std::endl;
     openvdb::initialize();
+
     // openvdb::points::PointAttributeVector<openvdb::Vec3R> positionsWrapper(points);
     const FloatBufferAttrVector<openvdb::Vec3R> positionsWrapper(points);
     float spacing = iniSpacing/2.f;
@@ -838,22 +840,24 @@ void createVoxelGrid(std::vector<float> points, ChSystemNSC& sys, std::shared_pt
 #else
 void createVoxelGrid(std::vector<float> points,
     ChSystemNSC& sys,
+    ChSystemFsi& sysFSI,
     std::shared_ptr<ChScene> scene,
     std::shared_ptr<ChVisualMaterial> vis_mat) {
-
-    std::cout << "Creating CPU Voxel Grid for " << points.size() / 6 << "particles " << std::endl;
+    std::vector<ChVector3d> chpoints = sysFSI.GetParticlePositions();
+    std::cout << "Creating CPU Voxel Grid for " << chpoints.size() << "particles " << std::endl;
     float spacing = iniSpacing / 2.f;
     float r = spacing / 2;
     int pointsPerVoxel = 1;
     float voxelSize = spacing;  
     std::cout << "VoxelSize=" << voxelSize << std::endl;
 
-    activeVoxels = points.size() / 6;
+    activeVoxels = chpoints.size();
     
     int numVoxelsToAdd = activeVoxels - prevActiveVoxels;
     int numAdds = 0;
     int numUpdates = 0;
 
+   
 
     if (!firstInst) {
         thread_local std::mt19937 generator(std::random_device{}());
@@ -862,7 +866,8 @@ void createVoxelGrid(std::vector<float> points,
         std::uniform_real_distribution<float> randscale(1.f, 1.5);
         int voxelCount = 0;
         for (int i = 0; i < activeVoxels; i++) {
-                 ChVector3d voxelPos(points[6*i],points[6*i + 1],points[6*i+2]);
+                //ChVector3d voxelPos(points[6*i],points[6*i + 1],points[6*i+2]);
+                ChVector3d voxelPos = chpoints[i];
                 if (!idList.empty() && ((voxelCount < prevActiveVoxels) || (voxelCount < idList.size()))) {
                     numUpdates++;
                     auto voxelBody = voxelBodyList[idList[voxelCount]];
@@ -938,7 +943,7 @@ void createVoxelGrid(std::vector<float> points,
 
         // Use std::for_each with parallel execution
         //std::for_each(std::execution::par, points.begin(), points.begin() + activeVoxels, [&](float& point) {
-        for (int i = 0; i < points.size()/6; i++) {
+        for (int i = 0; i < chpoints.size(); i++) {
             // Calculate the index based on the position in the loop
             //int i = &point - &points[0];  // Get the current index
 
@@ -947,7 +952,8 @@ void createVoxelGrid(std::vector<float> points,
             std::uniform_real_distribution<float> randpos(-.005f, .005f);
             std::uniform_real_distribution<float> randscale(1.f, 1.5);
             // Compute voxel position in world space
-            ChVector3d voxelPos(points[6 * i], points[6 * i + 1], points[6 * i + 2]);
+            //ChVector3d voxelPos(points[6 * i], points[6 * i + 1], points[6 * i + 2]);
+            ChVector3d voxelPos = chpoints[i];
             // Create voxelBody if necessary
             if (numVoxelsToAdd > 0 && i >= prevActiveVoxels) {
                 std::shared_ptr<ChBody> voxelBody;
