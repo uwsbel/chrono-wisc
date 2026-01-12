7277471f5f221ffa8059668565243c5d7ef22acf
BO-HSUN CHEN <bchen293@wisc.edu>
2026-01-13 02:34:55 +0800

    Updated the new path integrator

--- src/chrono_sensor/optix/shaders/camera_path_shader.cu
+++ src/chrono_sensor/optix/shaders/camera_path_shader.cu
@@ -25,6 +25,7 @@
 
 #include "chrono_sensor/optix/shaders/device_utils.h"
 #include "chrono_sensor/optix/shaders/shader_utils.cu"
+#include "chrono_sensor/optix/shaders/ChOptixLightClasses.cuh"
 
 static __device__ __inline__ void RussainRoulette(curandState_t& rng, float3& contrib_to_pixel) {
 	float p = fmaxf(0.05, fminf(fmaxf(contrib_to_pixel), 0.95));
@@ -32,7 +33,7 @@ static __device__ __inline__ void RussainRoulette(curandState_t& rng, float3& co
 }
 
 static __device__ __inline__ bool CheckPointLightVisible(
-	const ContextParameters& cntxt_params, PerRayData_camera* prd_camera, const Light& light, LightSample& light_sample
+	const ContextParameters& cntxt_params, const PerRayData_camera* prd_camera, const Light& light, LightSample& light_sample
 ) {
 	// Direction and distance from hit-point to light
 	light_sample.dir = light.pos - light_sample.hitpoint;
@@ -97,8 +98,9 @@ static __device__ __inline__ bool CheckPointLightVisible(
 		OptixVisibilityMask(1), OPTIX_RAY_FLAG_NONE, 0, 1, 0, opt1, opt2, raytype
 	);
 	*/
-	// light_sample.L = light.atten_scale * light.color / (light_sample.dist * light_sample.dist); // inverse square law
-	light_sample.L = light.color * (light.max_range * light.max_range / (light_sample.dist * light_sample.dist + light.max_range * light.max_range));
+	light_sample.L = light.color;
+	// light_sample.L = 170.f * 170.f * light.color / (light_sample.dist * light_sample.dist); // inverse square law
+	// light_sample.L = light.color * (light.max_range * light.max_range / (light_sample.dist * light_sample.dist + light.max_range * light.max_range));
 	light_sample.L = light_sample.L * light_sample.NdL; 	
 	// light_sample.L = light_sample.L * prd_shadow.attenuation * light_sample.NdL;
 	// Caculate the remaining attributes of light sample
@@ -193,7 +195,7 @@ static __device__ __inline__ void CameraPathIntegrator(
 		light_sample.hitpoint = hit_point;
 		light_sample.wo = -ray_dir;
 		light_sample.n = world_normal;
-		if(CheckLightVisible(cntxt_params, prd_camera, light, light_sample)) {
+		if(CheckVisibleAndSampleLight(cntxt_params, prd_camera, light_sample)) {
 			halfway = normalize(light_sample.dir - ray_dir);
 			NdH = Dot(world_normal, halfway);
 			VdH = Dot(-ray_dir, halfway); // dot(V, H) = dot(H, L), since H is halfway between L and V
@@ -214,108 +216,134 @@ static __device__ __inline__ void CameraPathIntegrator(
 	if (prd_camera->depth + 1 < params.max_depth) {
 		
 		// printf("Next ray!\n");
-		
-		float weights[2] = {0.5f, 0.5f}; // [diffuse weight, specular weight]
-		for (int weight_idx = 0; weight_idx < 2; ++weight_idx) {
-			float3 next_dir;
-			// Diffuse reflected ray
-			if (weight_idx == 0) {
-				// Just use cosine sampling to sample hemisphere for next ray
-				next_dir = sample_cosine_hemisphere_dir(curand_uniform(&prd_camera->rng), curand_uniform(&prd_camera->rng), world_normal);
-			}
-			// Specular reflected ray
-			else {
-				next_dir = normalize(reflect(ray_dir, world_normal));
-			}
-			
-			NdL = Dot(world_normal, next_dir);
-			float3 halfway = normalize(next_dir - ray_dir);
-			float NdH = Dot(world_normal, halfway);
-			float VdH = Dot(-ray_dir, halfway);  // Same as LdH
-
-			// Legacy dielectric Fresnel
-			
-			// float3 F = make_float3(0.0f);
-			// // === dielectric workflow
-			// if (mat.use_specular_workflow) {
-			// 	float3 specular = (mat.ks_tex) ? GetTexFloat4ValFloat3(mat.ks_tex, mat.tex_scale, uv) : mat.Ks;
-			// 	float3 F0 = specular * 0.08f;
-			// 	F = fresnel_schlick(VdH, 5.f, F0, make_float3(1.f));
-			// } else {
-			// 	float3 default_dielectrics_F0 = make_float3(0.04f);
-			// 	F = metallic * albedo + (1 - metallic) * default_dielectrics_F0;
-			// }
-
-			// float D = NormalDist(NdH, roughness);        // 1/pi omitted
-			// float G = HammonSmith(NdV, NdL, roughness);  // 4  * NdV * NdL omitted
-
-			// float3 f_ct = F * D * G;
-
-			// // Note only specular part appears here. Energy preserve
-			// // Since it is not random, PDF is 1 (normally 1/pi),
-			// // If the camera uses GI, then it will trace two rays. So each ray's contribution should be halfed
-
-			// // mirror correction accounts for us oversampling this direction
-			// // following line comes from a heuristic. Perect reflection for metalic smooth objects,
-			// // no reflection for rough non-metalic objects
-			// float mirror_correction = (1.f - roughness) * (1.f - roughness) * metallic * metallic;
-
-			// float3 partial_contrib = mirror_correction * f_ct * NdL / (4 * CUDART_PI_F);
-			// partial_contrib = clamp(partial_contrib, make_float3(0), make_float3(1));
-
-			// partial_contrib = partial_contrib * prd_camera->contrib_to_pixel;
-			// float3 next_contrib_to_pixel = clamp(partial_contrib, make_float3(0), make_float3(1));
-			
-			
-			float sampling_pdf = NdL / CUDART_PI_F; // cosine hemisphere pdf
-			float3 this_contrib_to_pixel = NdL * PrincipledBRDF(albedo, roughness, metallic, specular, 1.0f, mat.use_specular_workflow, NdV, NdL, NdH, VdH);
-			
-			// LEGACY: magic heuristic mirror correction for specular reflection
-			if (weight_idx == 1) 
-				this_contrib_to_pixel = this_contrib_to_pixel * (1.f - roughness) * (1.f - roughness) * metallic * metallic; 
-			
-			float3 next_contrib_to_pixel = prd_camera->contrib_to_pixel * this_contrib_to_pixel;
-			
-			// Go through Russian roulette (RR)
-			if (prd_camera->depth > 3) {
-				RussainRoulette(prd_camera->rng, next_contrib_to_pixel);
-			}
-
-			// Determine if terminating the next ray
-			if (luminance(next_contrib_to_pixel) > params.importance_cutoff) {
-			// if (fmaxf(next_contrib_to_pixel) > 0.01f) {
-				// Trace next ray
-				PerRayData_camera prd_reflection = DefaultCameraPRD();
-				prd_reflection.integrator = prd_camera->integrator;
-				prd_reflection.rng = prd_camera->rng;
-				prd_reflection.depth = prd_camera->depth + 1;
-				prd_reflection.use_gi = prd_camera->use_gi;
-				prd_reflection.contrib_to_pixel = next_contrib_to_pixel;
-				unsigned int opt1, opt2;
-				pointer_as_ints(&prd_reflection, opt1, opt2);
-				unsigned int raytype = static_cast<unsigned int>(RayType::CAMERA_RAY_TYPE);
-				optixTrace(
-					cntxt_params.root,          // The scene traversable handle (OptixTraversableHandle); basically the top-level acceleration structure (TLAS).
-					hit_point,					// origin of the traced ray
-					next_dir,           		// direction of the traced ray
-					cntxt_params.scene_epsilon, // minimum intersection distance to avoid self-intersection (“shadow acne”)
-					1e16f,          			// A very large max distance (effectively “infinite” for the scene scale)
-					optixGetRayTime(),          // time value for launching this ray
-					OptixVisibilityMask(1),     // Only intersects geometry whose instance mask matches 1
-					OPTIX_RAY_FLAG_NONE,		// terminate on first hit is ideal for occlusion rays
-					0,                          // SBT offset (used when you have multiple SBT records for the same ray type). It selects the first “ray type slot”
-					1,                          // SBT stride (used when you have multiple SBT records for the same ray type). It usually means “one ray type stride”
-					0,                          // missSBTIndex. It selects the first miss program
-					opt1,                       // Final payloads; the per-ray data pointer (first 32 bits); optixGetPayload_0() = opt1
-					opt2,                       // Final payloads; the per-ray data pointer (second 32 bits); optixGetPayload_1() = opt2
-					raytype                     // The ray type index (used when you have multiple ray types, e.g., radiance rays, shadow rays, etc.)
-				);
-				
-				// prd_camera->color += prd_reflection.color;
-				prd_camera->color += weights[weight_idx] * (prd_reflection.color * this_contrib_to_pixel) / (sampling_pdf);
-				// prd_camera->color += prd_reflection.color;
-			}
+		// int weight_idx_start = 2, weight_idx_end = 2;
+		// float weights[2] = {0.f, 0.f}; // [diffuse weight, specular weight]
+		float diffuse_prob = 0.;
+		if (bsdf_type == BSDFType::DIFFUSE) {
+			// weight_idx_start = 0;
+			// weight_idx_end = 1;
+			// weights[0] = 1.001f; // diffuse weight
+			// weights[1] = 0.f; // specular weight
+			diffuse_prob = 1.f;
+		} else if (bsdf_type == BSDFType::SPECULAR) {
+			// weight_idx_start = 1;
+			// weight_idx_end = 2;
+			// weights[0] = 0.f; // diffuse weight
+			// weights[1] = 1.f; // specular weight
+			diffuse_prob = 0.f;
+		} else {
+			// weight_idx_start = 0;
+			// weight_idx_end = 2;
+			// weights[0] = 0.5f; // diffuse weight
+			// weights[1] = 0.5f; // specular weight
+			diffuse_prob = 0.5f;
 		}
+
+		// Randomly choose diffuse or specular reflection based on weights. weight_idx = 0: diffuse, 1: specular
+		int weight_idx = (curand_uniform(&prd_camera->rng) <= diffuse_prob) ? 0 : 1;
+
+		// for (int weight_idx = weight_idx_start; weight_idx < weight_idx_end; ++weight_idx) {
+		float3 next_dir;
+		// Diffuse reflected ray
+		if (weight_idx == 0) {
+			// Just use cosine sampling to sample hemisphere for next ray
+			next_dir = sample_cosine_hemisphere_dir(curand_uniform(&prd_camera->rng), curand_uniform(&prd_camera->rng), world_normal);
+		}
+		// Specular reflected ray
+		else {
+			next_dir = normalize(reflect(ray_dir, world_normal));
+		}
+		
+		NdL = Dot(world_normal, next_dir);
+		float3 halfway = normalize(next_dir - ray_dir);
+		float NdH = Dot(world_normal, halfway);
+		float VdH = Dot(-ray_dir, halfway);  // Same as LdH
+
+		// Legacy dielectric Fresnel
+		
+		// float3 F = make_float3(0.0f);
+		// // === dielectric workflow
+		// if (mat.use_specular_workflow) {
+		// 	float3 specular = (mat.ks_tex) ? GetTexFloat4ValFloat3(mat.ks_tex, mat.tex_scale, uv) : mat.Ks;
+		// 	float3 F0 = specular * 0.08f;
+		// 	F = fresnel_schlick(VdH, 5.f, F0, make_float3(1.f));
+		// } else {
+		// 	float3 default_dielectrics_F0 = make_float3(0.04f);
+		// 	F = metallic * albedo + (1 - metallic) * default_dielectrics_F0;
+		// }
+
+		// float D = NormalDist(NdH, roughness);        // 1/pi omitted
+		// float G = HammonSmith(NdV, NdL, roughness);  // 4  * NdV * NdL omitted
+
+		// float3 f_ct = F * D * G;
+
+		// // Note only specular part appears here. Energy preserve
+		// // Since it is not random, PDF is 1 (normally 1/pi),
+		// // If the camera uses GI, then it will trace two rays. So each ray's contribution should be halfed
+
+		// // mirror correction accounts for us oversampling this direction
+		// // following line comes from a heuristic. Perect reflection for metalic smooth objects,
+		// // no reflection for rough non-metalic objects
+		// float mirror_correction = (1.f - roughness) * (1.f - roughness) * metallic * metallic;
+
+		// float3 partial_contrib = mirror_correction * f_ct * NdL / (4 * CUDART_PI_F);
+		// partial_contrib = clamp(partial_contrib, make_float3(0), make_float3(1));
+
+		// partial_contrib = partial_contrib * prd_camera->contrib_to_pixel;
+		// float3 next_contrib_to_pixel = clamp(partial_contrib, make_float3(0), make_float3(1));
+		
+		
+		float sampling_pdf = NdL / CUDART_PI_F; // cosine hemisphere pdf
+		float3 this_contrib_to_pixel = NdL * PrincipledBRDF(albedo, roughness, metallic, specular, 1.0f, mat.use_specular_workflow, NdV, NdL, NdH, VdH);
+		// float3 this_contrib_to_pixel = make_float3(1.f) * NdL;
+		
+		// LEGACY: magic heuristic mirror correction for specular reflection
+		if (weight_idx == 1) 
+			this_contrib_to_pixel = this_contrib_to_pixel * (1.f - roughness) * (1.f - roughness) * metallic * metallic; 
+		
+		float3 next_contrib_to_pixel = prd_camera->contrib_to_pixel * this_contrib_to_pixel;
+		
+		// Go through Russian roulette (RR)
+		if (prd_camera->depth > 3 && fmaxf(next_contrib_to_pixel) < 0.1f) {
+			RussainRoulette(prd_camera->rng, next_contrib_to_pixel);
+		}
+
+		// Determine if terminating the next ray
+		if (luminance(next_contrib_to_pixel) > params.importance_cutoff) {
+		// if (fmaxf(next_contrib_to_pixel) > 0.01f) {
+			// Trace next ray
+			PerRayData_camera prd_reflection = DefaultCameraPRD();
+			prd_reflection.integrator = prd_camera->integrator;
+			prd_reflection.rng = prd_camera->rng;
+			prd_reflection.depth = prd_camera->depth + 1;
+			prd_reflection.use_gi = prd_camera->use_gi;
+			prd_reflection.contrib_to_pixel = next_contrib_to_pixel;
+			unsigned int opt1, opt2;
+			pointer_as_ints(&prd_reflection, opt1, opt2);
+			unsigned int raytype = static_cast<unsigned int>(RayType::CAMERA_RAY_TYPE);
+			optixTrace(
+				cntxt_params.root,          // The scene traversable handle (OptixTraversableHandle); basically the top-level acceleration structure (TLAS).
+				hit_point,					// origin of the traced ray
+				next_dir,           		// direction of the traced ray
+				cntxt_params.scene_epsilon, // minimum intersection distance to avoid self-intersection (“shadow acne”)
+				1e16f,          			// A very large max distance (effectively “infinite” for the scene scale)
+				optixGetRayTime(),          // time value for launching this ray
+				OptixVisibilityMask(1),     // Only intersects geometry whose instance mask matches 1
+				OPTIX_RAY_FLAG_NONE,		// terminate on first hit is ideal for occlusion rays
+				0,                          // SBT offset (used when you have multiple SBT records for the same ray type). It selects the first “ray type slot”
+				1,                          // SBT stride (used when you have multiple SBT records for the same ray type). It usually means “one ray type stride”
+				0,                          // missSBTIndex. It selects the first miss program
+				opt1,                       // Final payloads; the per-ray data pointer (first 32 bits); optixGetPayload_0() = opt1
+				opt2,                       // Final payloads; the per-ray data pointer (second 32 bits); optixGetPayload_1() = opt2
+				raytype                     // The ray type index (used when you have multiple ray types, e.g., radiance rays, shadow rays, etc.)
+			);
+			
+			// prd_camera->color += prd_reflection.color;
+			// prd_camera->color += weights[weight_idx] * (prd_reflection.color * this_contrib_to_pixel) / (sampling_pdf);
+			prd_camera->color += (prd_reflection.color * this_contrib_to_pixel) / (sampling_pdf);
+			
+		}
+		// }
 
 		// BSDFSample sample;
 		// sample.wo = wo;
