9630584c5905f01efbb24ff74eb9166a41daf46d
DanielYamChen <r05522811@ntu.edu.tw>
2026-03-19 01:15:38 -0500

    Fixed number of ray recursions in `btest_SEN_instanced_meshes.cpp` (#713)
    * Fixed number of ray recursions in `btest_SEN_instanced_meshes.cpp`
    
    Updated the ray recursion setting for the sensor manager to allow for launching ray bounces during path tracing.
    
    * Activate denoiser

--- src/tests/benchmark_tests/sensor/btest_SEN_instanced_meshes.cpp
+++ src/tests/benchmark_tests/sensor/btest_SEN_instanced_meshes.cpp
@@ -93,7 +93,7 @@ int main(int argc, char* argv[]) {
         // Create a sensor manager
         // -----------------------
         auto manager = std::make_shared<ChSensorManager>(&sys);
-        manager->SetRayRecursions(0);
+        manager->SetRayRecursions(9); // number of ray bounces for tracing each path
         manager->scene->AddPointLight({100, 100, 100}, {1, 1, 1}, 5000);
         // manager->scene->AddPointLight({-100, 100, 100}, {1, 1, 1}, 5000);
         // manager->scene->AddPointLight({100, -100, 100}, {1, 1, 1}, 5000);
@@ -108,7 +108,7 @@ int main(int argc, char* argv[]) {
             chrono::ChFrame<double>({-10, 0, 0}, QuatFromAngleAxis(0, {0, 1, 0})),  // offset pose
             1920,                                                                   // image width
             1080,                                                                   // image height
-            (float)CH_PI / 3, 1, CameraLensModelType::PINHOLE, true                 // FOV
+            (float)CH_PI / 3, 1, CameraLensModelType::PINHOLE, true, true           // FOV, samples per pixel (spp), lens model type, consider diffuse reflection, use OptiX denoiser
         );
         cam->SetName("Camera Sensor");
         if (vis)
